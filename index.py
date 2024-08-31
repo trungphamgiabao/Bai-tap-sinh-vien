1b3fcd8cdd95a4eba053f32c1277122cf7a3b9e3
trungphamgiabao <142603462+trungphamgiabao@users.noreply.github.com>
2024-08-31 16:13:53 +0700

    Update index.py

--- index.py
+++ index.py
@@ -19,29 +19,32 @@ nv1= NhanVien("NV001","Nguyen dep trai", 1000, "Developer",3.5)
 print(nv1.hienthithongtin())
 
 class Quanly(NhanVien):
-    def __init__(self, manhanvien, tennhanvien, luongcoban, tienthuong, hesoluong, danhsachphongban):
-        super().__init__(manhanvien, tennhanvien, luongcoban, "Quản lý", hesoluong)
-        self.tienthuong=tienthuong
-        self.danhsachphongban=danhsachphongban
-    
+    def __init__(self, manhanvien, tennhanvien, luongcoban, ChucVu, hesoluong, bonus):
+        super().__init__(manhanvien, tennhanvien, luongcoban, ChucVu, hesoluong,)
+        self.bonus=bonus
     def tinhluong(self):
-        return super().tinhluong()+self.tienthuong
-    def hienthithongtin(self):
-        info=super().hienthithongtin()
-        return f"{info}, Phòng ban quản lý:{','.join(self.danhsachphongban)}"
-ql1=Quanly("QL001","Phan hanh phuc", 2000,4,500,["Ký thuật", "kế toán"])
+        return (super().tinhluong())+self.bonus
+quanly=Quanly(1,"quản lý",10,"QL",10,1000)
+def hienthithongtin(self):
+        return f"""
+                id: {self.manhanvien},
+                Tên: {self.tennhanvien},
+                Chức vụ: {self.chucvu},
+                Lương: {self.tinhluong()}"""
 
-class GiamDoc(NhanVien):
-    def __init__(self, manhanvien, tennhanvien, luongcoban, tienthuong, hesoluong, danhsachchinhanh):
-        super().__init__(manhanvien, tennhanvien, luongcoban, "Giám Đốc", hesoluong)
-        self.tienthuong=tienthuong
-        self.danhsachchinhanh=danhsachchinhanh
-    def tinhluong(self):
-        if len(self.danhsachchinhanh)>3:
-            return super().tinhluong()+self.tienthuong+1000
-        else:
-            return super().tinhluong()+self.tienthuong
-    def hienthithongtin(self):
-        info= super().hienthithongtin()
-        return f"{info}, Chi nhánh quản lý:{','.join(self.danhsachchinhanh)}"
-gd1=GiamDoc("GD001","Nguyen thi thanh cong",5000,5,2000,["Chi nhánh A","Chi nhánh B","Chi nhánh C","Chi nhánh D"])
+print(quanly.hienthithongtin())
+
+class Giamdoc(NhanVien):
+    def __init__(self, manhanvien, tennhanvien, luongcoban, chucvu, hesoluong):
+        super().__init__(manhanvien, tennhanvien, luongcoban, chucvu, hesoluong)
+        
+giamdoc=Giamdoc(1,"giám 1",5,"GD",10)
+def hienthithongtin(self):
+        return f"""
+                id: {self.manhanvien},
+                Tên: {self.tennhanvien},
+                Chức vụ: {self.chucvu},
+                Lương: {self.tinhluong()}"""
+
+print(giamdoc.hienthithongtin())
+
